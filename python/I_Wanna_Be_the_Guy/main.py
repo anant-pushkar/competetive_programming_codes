14db6a615c18f0c5c62993a87e50a081299f0c79
anant-pushkar <anantpushkar009@gmail.com>
2014-09-21 18:08:58 +0530

    codeforces 268

--- python/I_Wanna_Be_the_Guy/main.py
+++ python/I_Wanna_Be_the_Guy/main.py
@@ -19,6 +19,8 @@ n = int(raw_input())
 freq = [0 for x in xrange(n)]
 i1 = [int(x)-1 for x in str(raw_input()).split(" ")]
 i2 = [int(x)-1 for x in str(raw_input()).split(" ")]
+i1 = i1[1::] if i1[0]!=-1 else []
+i2 = i2[1::] if i2[0]!=-1 else []
 for i in i1:
 	freq[i] += 1
 for i in i2:
